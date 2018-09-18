ff6f7db6bad2b0b6e3728a3acf1b49e0ad58f8b9
zz864751629 <43335120+zz864751629@users.noreply.github.com>
2018-09-18 16:47:32 +0800

    Create second.cpp

---
+++ w02/second.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	int sec;
+	int hh,mm,ss;
+	printf("Enter the second :");
+	scanf("%d",&sec);
+	
+	hh = sec / 3600 ;
+	mm = sec % 3600 / 60;
+	ss = sec % 60;
+	
+	printf("%d second = %02d:%02d:%02d\n",sec,hh,mm,ss);
+	
+	
+}
